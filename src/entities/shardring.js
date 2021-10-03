19742ac8ca79adde17c01e9c5c773470a49d79c2
Robbie D <deibbor@gmail.com>
2021-10-03 18:26:47 -0500

    Shards accurately displaying entropy.

--- src/entities/shardring.js
+++ src/entities/shardring.js
@@ -6,9 +6,15 @@ export class ShardRing {
     constructor(scene) {
         this.scene = scene;
 
+        this.currentEntropy = character.entropy;
+        this.capacity = character.entropyCapacity;
+
         this.points = character.entropyCapacity;
+
         this.radius = 120;
+
         this.pointPool = [];
+        this.shardPool = [];
 
         this.createEntropy();
         this.setupListeners();
@@ -24,7 +30,11 @@ export class ShardRing {
 
     createEntropy() {
         console.log(`Creating ${character.entropyCapacity} shards with ${character.entropy} active.`);
+        this.createShardPoints();
+        this.createShards();
+    }
 
+    createShardPoints() {
         for(let i = 0; i < this.points; i++) {
             const point = {
                 x: this.radius * Math.cos((i * 2 * Math.PI) / this.points),
@@ -33,13 +43,45 @@ export class ShardRing {
             this.pointPool.push(point);
         }
 
-        this.pointPool.forEach((p) => {
-            this.scene.add.sprite(p.x + (1024 / 2), p.y + (768 / 2), "shard");
-        })
+    }
 
+    createShards() {
+        this.pointPool.forEach((p) => {
+            const shard = this.scene.add.sprite(p.x + (1024 / 2), p.y + (768 / 2), "shard");
+            shard.setAlpha(0.3);
+            shard.activeShard = false;
+            this.shardPool.push(shard);
+        })
+    }
+
+    destroyShards() {
+        this.shardPool.forEach((shard) => {
+            shard.destroy();
+        })
     }
 
     updateEntropy() {
+        if(this.capacity !== character.entropyCapacity) {
+            this.capacity = character.entropyCapacity;
+
+            this.pointPool = [];
+            this.createShardPoints();
+            this.destroyShards();
+            this.createShards();
+        } else if(this.currentEntropy !== character.entropy) {
+            this.currentEntropy = character.entropy
+
+
+
+            this.shardPool.forEach((shard, index) => {
+                console.log(index, this.currentEntropy);
+                if(index < this.currentEntropy) {
+                    shard.setAlpha(1);
+                } else {
+                    shard.setAlpha(0.3);
+                }
+            });
+        }
     }
 
     cleanup() {
