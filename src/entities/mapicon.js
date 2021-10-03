0b766903d4740ea312ea8297e271e1003a74c03a
Robbie D <deibbor@gmail.com>
2021-10-02 22:13:14 -0500

    Early workings on map icon transformation, real asset imports.

--- src/entities/mapicon.js
+++ src/entities/mapicon.js
@@ -12,12 +12,31 @@ export class MapIcon {
         this.mapPosition = mapPosition;
         this.state = 1;
 
-        
+        this.setupListeners();
         this.addToScene();
     }
 
+    setupListeners() {
+        this.onStatsUpdated = () => {
+            this.statsUpdated();
+        };
+
+        eventBus.on("game:statsUpdated", this.onStatsUpdated);
+
+    }
+
+    statsUpdated() {
+        if(this.type !== "HOME") {
+            const newState = this.calculateState();
+            if(newState !== this.state) {
+                this.state = newState;
+                this.sprite.setTexture(`${this.type}-${this.state}`);
+            }
+        }
+    }
+
     addToScene() {
-        this.sprite = this.scene.add.sprite(this.x, this.y, "darkblue");
+        this.sprite = this.scene.add.sprite(this.x, this.y, `${this.type}-${this.state}`);
         this.sprite.setScale(0.5);
 
         this.sprite.setInteractive();
@@ -39,7 +58,26 @@ export class MapIcon {
     }
 
     calculateState() {
+        const stat = character.stats[this.type];
+        let newState = this.state;
 
+        if(stat <= 2) {
+            newState = 1;
+        } else if(stat <= 4) {
+            newState = 2;
+        } else if(stat <= 6) {
+            newState = 3;
+        } else if(stat <= 8) {
+            newState = 4;
+        } else if(stat >= 10) {
+            newState = 5;
+        }
+
+        return newState;
+    }
+
+    cleanup() {
+        eventBus.off("game:statsUpdated", this.onStatsUpdated);
     }
 
 }
